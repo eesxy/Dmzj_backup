752fa36e4ec30c7a22fb45603b18ac736b622e6e
eesxy <eesxy@users.noreply.github.com>
2021-03-13 21:46:40 +0800

    v0.1.1
    1.修复了漫画名或章节名含有非法字符导致创建文件目录失败的问题
    2.增加了对原创漫画的解析

--- Dmzj_backup/spiders/Dmzj.py
+++ Dmzj_backup/spiders/Dmzj.py
@@ -14,6 +14,7 @@ class DmzjSpider(scrapy.Spider):
     name = 'Dmzj'
     image_base_url = 'https://images.dmzj.com/'
     chapter_base_url = 'http://manhua.dmzj.com'
+    original_image_base_url = 'https://images.dmzj1.com/'
 
     def __init__(self):
         logger = logging.getLogger()
@@ -36,25 +37,37 @@ class DmzjSpider(scrapy.Spider):
                          (len(comic_list)))
 
         for url in comic_list:
-            yield scrapy.FormRequest(url, callback=self.parse_comic)
+            host = re.sub(r'(http://|https://)', '', url).split('/')[0]
+            if host == 'www.dmzj.com':
+                yield scrapy.FormRequest(url, callback=self.parse_original_comic)
+            elif host == 'manhua.dmzj.com':
+                yield scrapy.FormRequest(url, callback=self.parse_comic)
+            else:
+                raise NotImplementedError
 
     def parse_comic(self, response):
         cover_url = response.css(
             '.anim_intro_ptext > a > img::attr(src)').get()
         comic_name = response.css('h1::text').get()
         last_updated = response.css('.update2::text').get()
+        comic_name = re.sub(r'(^\s*|\s*$|[/\?*:"<>|])', '', comic_name)
 
         chapter_list = []
+        record_chapter_list = []
         raw_url_list = response.css(
             '.cartoon_online_border > ul > li > a')
         for url in raw_url_list:
+            chapter_name = re.sub(r'(^\s*|\s*$|[/\?*:"<>|])', '', url.css('::text').get())
             chapter_list.append(
-                (self.chapter_base_url+url.attrib['href'], url.css('::text').get()))
+                (self.chapter_base_url+url.attrib['href'], chapter_name))
+            record_chapter_list.append(chapter_name)
         raw_url_list = response.css(
             '.cartoon_online_border_other > ul > li > a')
         for url in raw_url_list:
+            chapter_name = re.sub(r'(^\s*|\s*$|[/\?*:"<>|])', '', url.css('::text').get())
             chapter_list.append(
-                (self.chapter_base_url+url.attrib['href'], url.css('::text').get()))
+                (self.chapter_base_url+url.attrib['href'], chapter_name))
+            record_chapter_list.append(chapter_name)
         self.logger.info('Parsed comic: %s, found %d chapter(s)' %
                          (comic_name, len(chapter_list)))
 
@@ -65,10 +78,42 @@ class DmzjSpider(scrapy.Spider):
                 yield CoverItem(comic_name=comic_name, cover_url=cover_url)
         else:
             yield CoverItem(comic_name=comic_name, cover_url=cover_url)
+
         for chapter in chapter_list:
             yield scrapy.FormRequest(chapter[0], callback=self.parse_chapter,
                                      cb_kwargs=dict(comic_name=comic_name, chapter_name=chapter[1]))
-        record_chapter_list = [chapter[1] for chapter in chapter_list]
+        yield ComicItem(comic_name=comic_name, last_updated=last_updated, chapter_list=record_chapter_list)
+
+    def parse_original_comic(self, response):
+        cover_url = response.css(
+            '.comic_i_img > a > img::attr(src)').get()
+        comic_name = response.css('.comic_deCon > h1 > a::text').get()
+        last_updated = response.css('.zj_list_head_dat::text').get()
+        comic_name = re.sub(r'(^\s*|\s*$|[/\?*:"<>|])', '', comic_name)
+
+        chapter_list = []
+        record_chapter_list = []
+        raw_url_list = response.css(
+            '.tab-content.zj_list_con.autoHeight > ul > li > a')
+        for url in raw_url_list:
+            chapter_name = re.sub(r'(^\s*|\s*$|[/\?*:"<>|])', '',url.css('::text').get())
+            chapter_list.append(
+                (url.attrib['href'], chapter_name))
+            record_chapter_list.append(chapter_name)
+        self.logger.info('Parsed comic: %s, found %d chapter(s)' %
+                         (comic_name, len(chapter_list)))
+
+        if MY_UPDATE_MODE:
+            chapter_list = self.check_update(
+                comic_name, last_updated, chapter_list)
+            if not os.path.exists(os.path.join(IMAGES_STORE, comic_name, 'cover.jpg')):
+                yield CoverItem(comic_name=comic_name, cover_url=cover_url)
+        else:
+            yield CoverItem(comic_name=comic_name, cover_url=cover_url)
+
+        for chapter in chapter_list:
+            yield scrapy.FormRequest(chapter[0], callback=self.parse_original_chapter,
+                                     cb_kwargs=dict(comic_name=comic_name, chapter_name=chapter[1]))
         yield ComicItem(comic_name=comic_name, last_updated=last_updated, chapter_list=record_chapter_list)
 
     def check_update(self, comic_name, last_updated, chapter_list):
@@ -83,7 +128,9 @@ class DmzjSpider(scrapy.Spider):
             downloaded_chapter_list = info_dict['chapter_list']
             update_list = []
             for chapter in chapter_list:
-                if chapter[1] not in downloaded_chapter_list:
+                chapter_name = re.sub(
+                    r'(^\s*|\s*$|[/\?*:"<>|])', '', chapter[1])
+                if chapter_name not in downloaded_chapter_list:
                     update_list.append(chapter)
             self.logger.info('%d new chapter(s) in %s' %
                              (len(update_list), comic_name))
@@ -97,7 +144,27 @@ class DmzjSpider(scrapy.Spider):
         raw_url_list = re.sub(r'[\[\"\]]', '', raw_url_list).split(',')
         img_list = []
         for url in raw_url_list:
+            url = re.sub(r'^/+', '', url)
             img_list.append(self.image_base_url+url)
+
+        self.logger.info('Parsed chapter: %s %s, found %d image(s)' %
+                         (comic_name, chapter_name, len(img_list)))
+        for url in img_list:
+            yield ImgItem(comic_name=comic_name, chapter_name=chapter_name,
+                          img_name=urllib.parse.unquote(url.split('/')[-1], 'utf-8'), img_url=url)
+
+    def parse_original_chapter(self, response, comic_name, chapter_name):
+        eval_script = response.css('head > script::text').re_first('eval(.*)')
+        eval_script = eval_script.replace('&lt;', '<').replace('&gt;', '>')
+
+        raw_url_dict = js2py.eval_js('eval'+eval_script)
+        raw_url_dict = re.sub(r'\s+', ' ', raw_url_dict)
+        raw_url_dict = json.loads(raw_url_dict)
+        raw_url_list = raw_url_dict['page_url'].split(' ')
+        img_list = []
+        for url in raw_url_list:
+            url = re.sub(r'^/+', '', url)
+            img_list.append(self.original_image_base_url+url)
 
         self.logger.info('Parsed chapter: %s %s, found %d image(s)' %
                          (comic_name, chapter_name, len(img_list)))
